6989c419982d8de935fa9e10ff41ed9edab68438
Raymond Song <raymond.song98@gmail.com>
2024-11-17 22:00:42 -0500

    Add lazy vim installation

---
+++ install_lazyvim.sh
@@ -0,0 +1,31 @@
+#!/usr/bin/bash
+
+# Install Neovim From: github.com/basecamp/omakub
+cd /tmp
+wget -O nvim.tar.gz "https://github.com/neovim/neovim/releases/latest/download/nvim-linux64.tar.gz"
+tar -xf nvim.tar.gz
+sudo install nvim-linux64/bin/nvim /usr/local/bin/nvim
+sudo cp -R nvim-linux64/lib /usr/local/
+sudo cp -R nvim-linux64/share /usr/local/
+rm -rf nvim-linux64 nvim.tar.gz
+cd -
+
+# Only attempt to set configuration if Neovim has never been run
+if [ ! -d "$HOME/.config/nvim" ]; then
+  # Use LazyVim
+  git clone https://github.com/raymondsong00/lazyconfig ~/.config/nvim
+  # Remove the .git folder, so you can add it to your own repo later
+  rm -rf ~/.config/nvim/.git
+fi
+
+# Install things for Treesitter
+sudo apt update && sudo apt install fd-find ripgrep clangd
+
+# Install LazyGit
+cd /tmp
+LAZYGIT_VERSION=$(curl -s "https://api.github.com/repos/jesseduffield/lazygit/releases/latest" | grep -Po '"tag_name": "v\K[^"]*')
+curl -sLo lazygit.tar.gz "https://github.com/jesseduffield/lazygit/releases/latest/download/lazygit_${LAZYGIT_VERSION}_Linux_x86_64.tar.gz"
+tar -xf lazygit.tar.gz lazygit
+sudo install lazygit /usr/local/bin
+rm lazygit.tar.gz lazygit
+cd -
